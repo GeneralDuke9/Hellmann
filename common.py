372d07a49f2b54edb7c9e6383417ecc2d6d18a25
Stan Jakobs <stanjakobs@gmail.com>
2021-11-28 20:08:59 +0000

    Change code for new KNMI interface

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Update:
     station: str
     value: int
-
-    def __init__(self, _station: str, _value: int):
-        self.station = _station
-        self.value = _value
